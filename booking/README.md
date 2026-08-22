b67ea7d1d0ad3055ef104a7869349f9e1617a745
ilfat aliev <ialiev685@gmail.com>
2026-08-22 19:11:37 +0300

    init tables rooms and hotels

--- booking/README.md
+++ booking/README.md
@@ -2,3 +2,4 @@ alembic init [директория с миграциями] - инициация
 alembic revision -m [название миграции] - создать шаблон миграции
 alembic revision --autogenerate -m [название миграции] - создать миграцию на основе моделей
 alembic upgrade head - применить последнюю миграцию
+alembic downgrade [номер ревизии] - откатить миграцию (base в самое начало)
---
+++ booking/src/models/rooms.py
@@ -0,0 +1,14 @@
+from sqlalchemy import String, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+from src.database import Base
+
+
+class RoomsModel(Base):
+    __tablename__ = "rooms"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(length=30))
+    description: Mapped[str | None]
+    hotel_id: Mapped[int] = mapped_column(ForeignKey("hotels.id"))
+    price: Mapped[int]
+    quantity: Mapped[int]
